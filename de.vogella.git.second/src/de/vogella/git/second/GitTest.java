78248ff3cfdcafe8fb9f9dff46e0fd4c3ddc3412
jingood2 <jingood2@gmail.com>
2012-12-10 22:26:50 +0900

    Changed output message

--- de.vogella.git.second/src/de/vogella/git/second/GitTest.java
+++ de.vogella.git.second/src/de/vogella/git/second/GitTest.java
@@ -7,7 +7,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git Test Second!!!");
+		System.out.println("Git is Cool!!!");
 
 	}
 
